17260d9b8572093932e8a552dc9b05b36f2066ac
Bismuth <eliebismuth91@gmail.com>
2022-05-04 11:53:28 +0200

    start PromiseLibrary

--- PromiseLibrary/MyPromise.test.js
+++ PromiseLibrary/MyPromise.test.js
@@ -1,56 +1,99 @@
-// const MyPromise = require('./MyPromise');
-const MyPromise = Promise;
+const MyPromise = require("./MyPromise.js")
+// const MyPromise = Promise
 
-const DEFAULT_VALUE = "default";
+const DEFAULT_VALUE = "default"
 
 describe("then", () => {
-
   it("with no chaining", () => {
-    return promise().then(v => expect(v).toEqual(DEFAULT_VALUE));
-  });
+    return promise().then(v => expect(v).toEqual(DEFAULT_VALUE))
+  })
 
-  // With chaining
-  // With then and catch
-  // Multiple then
+  it("with multiple thens for same promise", () => {
 
-});
+  })
+
+  it("with then and catch", () => {
+
+  })
+
+  it("with chaining", () => {
+
+  })
+})
 
 describe("catch", () => {
+  it("with no chaining", () => {
+  })
 
-});
+  it("with multiple catches for same promise", () => {
+
+  })
+
+  it("with chaining", () => {
+
+  })
+})
 
 describe("finally", () => {
+  it("with no chaining", () => {
 
-});
+  })
+
+  it("with multiple finallys for same promise", () => {
+
+  })
+
+  it("with chaining", () => {
+
+  })
+})
 
 describe("static methods", () => {
-
   it("resolve", () => {
 
-  });
+  })
 
   it("reject", () => {
 
-  });
+  })
 
-  it("all", () => {
+  describe("all", () => {
+    it("with success", () => {
 
-  });
+    })
 
-  it("race", () => {
+    it("with fail", () => {
 
-  });
+    })
+  })
 
   it("allSettled", () => {
 
-  });
+  })
 
-});
+  describe("race", () => {
+    it("with success", () => {
 
+    })
+
+    it("with fail", () => {
+
+    })
+  })
+
+  describe("any", () => {
+    it("with success", () => {
+
+    })
+
+    it("with fail", () => {
+
+    })
+  })
+})
 
 function promise({ value = DEFAULT_VALUE, fail = false } = {}) {
-  return new Promise((resolve, reject) => {
-    fail ? reject(value) : resolve(value);
+  return new MyPromise((resolve, reject) => {
+    fail ? reject(value) : resolve(value)
   })
 }
-
